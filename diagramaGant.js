2fbf2d3b8aa488e340c29f64ce62f1563ce604db
Voskisimo <voskogrim@gmail.com>
2019-04-02 20:31:44 -0600

    moviendole a getTiempoRestante

--- diagramaGant.js
+++ diagramaGant.js
@@ -59,7 +59,38 @@ const Tarea = (function()
 
 		getTiempoRestante()
 		{
+			//Verificamos si el año de la fecha Inicial es el mismo de la fecha Termino 
+			if(_fechaInicio.get(this).getFullYear() != _fechaTermino.get(this).getFullYear())
+			{
+				//Si no es el mismo año, se resta la Termino a la Inicial y lo guardamos
+				let aniosRes = _fechaTermino.get(this).getFullYear() - _fechaInicio.get(this).getFullYear();
+			}
 
+			//Verificamos si el mes de la fecha Inicial es el mismo de la fecha Termino 
+			if (_fechaInicio.get(this).getMonth() != _fechaTermino.get(this).getMonth()) 
+			{
+				//Si no es el mismo mes, se resta la Termino a la Inicial y lo guardamos
+				let mesesRes = _fechaTermino.get(this).getMonth() - _fechaInicio.get(this).getMonth();
+				
+				//si es negativo, lo hacemos positivo
+				if (mesesRes<0) 
+				{
+					mesesRes = mesesRes * (-1);
+				}	
+			}
+
+			//Verificamos si el dia de la fecha Inicial es el mismo de la fecha Termino
+			if (_fechaInicio.get(this).getDate() != _fechaTermino.get(this).getDate()) 
+			{
+				//Si no es el mismo dia, se resta la Termino a la Inicial y lo guardamos
+				let diasRes = _fechaTermino.get(this).getDate() - _fechaInicio.get(this).getDate();
+				
+				//si es negativo, lo hacemos positivo
+				if (diasRes<0) 
+				{
+					diasRes = diasRes * (-1);
+				}	
+			}
 		}
 
 		agregarTarea()
@@ -108,5 +139,5 @@ function crearTarea()
 	let fecha2 = new Date(2020, 5, 12);
 	let tarea = new Tarea(0, fecha1, fecha2, "Tarea1", 10, null, null); //crear objeto
 
-	alert(tarea.getFechaInicio().getDate());
+	alert(tarea.getFechaInicio());
 }
